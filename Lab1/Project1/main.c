2bbc373a0416b01ef4ca4d95c557571e351643e6
Chris Cannon <Chris Cannon>
2017-02-28 06:28:18 -0500

    Got some assembly

--- Lab1/Project1/main.c
+++ Lab1/Project1/main.c
@@ -5,8 +5,13 @@ int d = 0;
 /*
  * main.c
  */
-int main(void) {
+void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
-	return 0;
+    int a =1;
+    float b = -255.25;
+	char c = 'c';
+	d = d+1;
+
+	while(1);
 }
---
+++ Lab1/Project2/main.asm
@@ -0,0 +1,45 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,WDTCTL  ; Stop watchdog timer
+mov.b #11h,R4
+mov.w #00AAh,R5
+and.w R4,R5
+
+jmp$
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+                                            
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            .end
+            
