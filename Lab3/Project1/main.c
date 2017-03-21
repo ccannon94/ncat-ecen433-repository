9ef8f1f615e8dbd8bc30f00406aecdda64ac6ad7
Chris Cannon <Chris Cannon>
2017-03-21 01:30:17 -0400

    Complete part 1

---
+++ Lab3/Project1/main.c
@@ -0,0 +1,20 @@
+#include <msp430.h> 
+
+int fib [100];
+
+/*
+ * main.c
+ */
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+    int i;
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for(i = 2; i < 100; i = i +1){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    while(1);
+}
