e6cfa080e098474ec72cc563cf9266d53724d898
Chris Cannon <Chris Cannon>
2017-03-21 01:56:09 -0400

    Edit Project2 from Project1

--- Lab3/Project2/main.c
+++ Lab3/Project2/main.c
@@ -8,5 +8,13 @@ float fib[200];
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    volatile int i;
+        fib[0] = 5;
+        fib[1] = 8;
 
+        for(i = 2; i < 200; i = i +1){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+        while(1);
 }
