f508f3689454c8d7656d4bb6a8cfdd5b29ef04e8
Chris Cannon <Chris Cannon>
2017-03-29 19:30:14 -0400

    Complete project 3

--- Lab4/Project3/main.c
+++ Lab4/Project3/main.c
@@ -3,8 +3,17 @@
 /*
  * main.c
  */
-int main(void) {
+void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
-	return 0;
+    volatile int a = 0;
+    volatile int mask = 0x0003;
+    volatile int y = 0xFFFF;
+
+    for(a = 0; a < 10; a++)
+    {
+        y = (y^mask)&a;
+    }
+
+    while(1);
 }
