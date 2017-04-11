0a7bb3047ce90602471b2baa7da950b4243da826
Chris Cannon <chris.c.cannon@gmail.com>
2017-04-11 10:06:09 -0400

    Complete-ish Project 2

--- Lab7/Project2/main.c
+++ Lab7/Project2/main.c
@@ -1,10 +1,24 @@
 #include <msp430.h> 
 
+#define LED BIT6
+#define BUTTON BIT3
+
 /*
  * main.c
  */
-int main(void) {
+void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
-	return 0;
+	P1DIR = LED;
+	P1OUT = 0x00;
+
+	while(1){
+	    if((P1IN & BUTTON) == 0x00){
+	        __delay_cycles(5000);
+	        if((P1IN & BUTTON) == 0x00){
+	            P1OUT ^=LED;
+	            while((P1IN & BUTTON) == 0x00);
+	        }
+	    }
+	}
 }
