9d4a705ef8f794dea2418e8d6be2d2631f426b73
Chris Cannon <Chris Cannon>
2017-04-17 21:31:07 -0400

    Builds

--- Lab1/BlinkLED/Debug/blink.d
+++ Lab1/BlinkLED/Debug/blink.d
@@ -4,12 +4,12 @@ blink.obj: ../blink.c
 blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
 blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
 blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
-blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h
-blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h
+blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h
+blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h
 
 ../blink.c: 
 C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
 C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
 C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h: 
--- Lab7/Project3/Debug/subdir_rules.mk
+++ Lab7/Project3/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 main.obj: ../main.asm $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/bin/cl430" -vmsp --code_model=small --data_model=small --use_hw_mpy=none --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/Users/chris/Documents/development/ncat-ecen433-repository/Lab7/Project3" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/bin/cl430" -vmsp --code_model=small --data_model=small --use_hw_mpy=none --include_path="C:/ti/ccsv7/ccs_base/msp430/include" --include_path="C:/Users/CCannon/Documents/development/ncat-ecen433-repository/Lab7/Project3" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
