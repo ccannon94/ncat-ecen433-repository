c470f1be42242278ed8075367de418085d1bf8be
Chris Cannon <Chris Cannon>
2017-04-23 20:08:47 -0400

    Learning IO

--- Lab8/Project1/main.c
+++ Lab8/Project1/main.c
@@ -9,12 +9,12 @@ void main(void) {
     //Set Pin 1.0 (LED1) to output, all others input
 	P1DIR = 0x01;
 	//Set LED1 to off
-	P1OUT = 0x00;
+	P1OUT = 0x01;
 
 	while(1){
-	    if(1)
+	    if((P1IN && 0x08) == 0x00)
 	    {
-
+	        P1OUT = 0x00;
 	    }else
 	    {
 
